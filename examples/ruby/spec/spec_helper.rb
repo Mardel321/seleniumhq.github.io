8a2853e7da4be16e85411d65117aff485fd497a5
titusfortner <titus.fortner@gmail.com>
2025-04-14 09:35:12 -0700

    I can spell

--- examples/ruby/spec/spec_helper.rb
+++ examples/ruby/spec/spec_helper.rb
@@ -32,10 +32,10 @@ RSpec.configure do |config|
 
   def start_session
     @service = Selenium::WebDriver::Service.chrome
-    @driver = Selenium::WebDriver.for(:chrome, options: deault_chrome_options)
+    @driver = Selenium::WebDriver.for(:chrome, options: default_chrome_options)
   end
 
-  def deault_chrome_options
+  def default_chrome_options
     options = Selenium::WebDriver::Chrome::Options.new
     options.add_argument('disable-search-engine-choice-screen')
     options.add_argument('--no-sandbox')
